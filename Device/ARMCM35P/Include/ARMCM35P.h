c908d509d612f6cb20d4b469c0ed8fefcfda3ea4
Jonatan Antoni <jonatan.antoni@arm.com>
2023-06-22 14:00:56 +0200

    Cortex-M35P: Fixed FPU and DSP availability.

--- Device/ARMCM35P/Include/ARMCM35P.h
+++ Device/ARMCM35P/Include/ARMCM35P.h
@@ -97,8 +97,8 @@ typedef enum IRQn
 #define __VTOR_PRESENT            1U        /* VTOR present */
 #define __NVIC_PRIO_BITS          3U        /* Number of Bits used for Priority Levels */
 #define __Vendor_SysTickConfig    0U        /* Set to 1 if different SysTick Config is used */
-#define __FPU_PRESENT             0U        /* no FPU present */
-#define __DSP_PRESENT             0U        /* no DSP extension present */
+#define __FPU_PRESENT             1U        /* SP FPU present */
+#define __DSP_PRESENT             1U        /* DSP extension present */
 
 #include "core_cm35p.h"                     /* Processor and core peripherals */
 #include "system_ARMCM35P.h"                /* System Header */
